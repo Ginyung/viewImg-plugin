f865568b5d766b94f1100707edd6c5131d75a0b2
Genyung <yjydnb@gmail.com>
2022-05-27 20:26:49 +0800

    master2

--- js/viewImg.js
+++ js/viewImg.js
@@ -27,9 +27,9 @@ function imgView(domName, parentsName) {
         });
     };
 
-
     /* 添加遮罩层 */
     function addMask() {
+
         var str_mask =
             "<div class=\"mask-layer\">" +
             "<div class=\"toolbar-soh iconfont icon-yincanggongjulan\" title=\"隐藏工具栏\"></div>" +
@@ -40,7 +40,7 @@ function imgView(domName, parentsName) {
             "<li><i class=\"iconfont icon-xiangyouxuanzhuan\" title=\"向右旋转\"></i></li>" +
             "<li><i class=\"iconfont icon-xiangzuoxuanzhuan\" title=\"向左旋转\"></i></li>" +
             "<li><i class=\"iconfont icon-open-blank\" title=\"新窗口打开\"></i></li>" +
-            "<li><i class=\"iconfont icon-huanyuan\" title=\"图片还原\"></i></li>" +
+            "<li><i class=\"iconfont icon-shuaxin\" title=\"图片还原\"></i></li>" +
             "<li><i class=\"iconfont icon-guanbi\" title=\"关闭\"></i></li>" +
             "</ul>" +
             "</div>" +
@@ -76,16 +76,16 @@ function imgView(domName, parentsName) {
                 judeg_soh = false;
             }
         });
-        
+
     };
-    function showCurImgNum(){
-        var img_num = img_index+1;
-        $(".cur-img-num").text("第"+img_num+"张 - 共"+len+"张");
-        $(".mask-layer-toolbar").mouseenter(function(){
-            $(".cur-img-num").css("bottom","0px");
+    function showCurImgNum() {
+        var img_num = img_index + 1;
+        $(".cur-img-num").text("第" + img_num + "张 - 共" + len + "张");
+        $(".mask-layer-toolbar").mouseenter(function () {
+            $(".cur-img-num").css("bottom", "0px");
         });
-        $(".mask-layer-toolbar").mouseleave(function(){
-            $(".cur-img-num").css("bottom","-48px");
+        $(".mask-layer-toolbar").mouseleave(function () {
+            $(".cur-img-num").css("bottom", "-48px");
         });
     };
 
@@ -100,7 +100,7 @@ function imgView(domName, parentsName) {
         /* 图片拖拽 */
         //绑定鼠标左键按住事件
         $img_container.bind("mousedown", function (event) {
-            event.preventDefault && event.preventDefault(); //去掉图片拖动响应
+            event.preventDefault && event.preventDefault(); //如果有preventDefault属性就执行该方法，去掉图片拖动响应
             //获取需要拖动节点的坐标
             var offset_x = $(this)[0].offsetLeft; //x坐标
             var offset_y = $(this)[0].offsetTop; //y坐标
@@ -110,7 +110,7 @@ function imgView(domName, parentsName) {
             var mouse_y = event.pageY;
             //绑定拖动事件
             //由于拖动时，可能鼠标会移出元素，所以应该使用全局（document）元素
-            $(".mask-layer").bind("mousemove", function (ev) {
+            $(document).bind("mousemove", function (ev) {
                 // 计算鼠标移动了的位置
                 var _x = ev.pageX - mouse_x;
                 var _y = ev.pageY - mouse_y;
@@ -124,24 +124,25 @@ function imgView(domName, parentsName) {
                 });
             });
             //当鼠标左键松开，解除事件绑定
-            $(".mask-layer").bind("mouseup", function () {
-                $(".mask-layer").unbind("mousemove");
+            $(document).bind("mouseup", function () {
+                $(document).unbind("mousemove").unbind("mouseup");
             });
+            return false;//去掉图片拖动响应
         });
 
 
         /* 图片缩放 */
         //绑定鼠标滚轮缩放图片
         $img_container.bind("mousewheel DOMMouseScroll", function (e) {
-            e = e || window.event;
+            e = e || window.event;//处理IE8兼容性问题
             var delta = e.originalEvent.wheelDelta || e.originalEvent.detail;
             var dir = delta > 0 ? 'down' : 'up';
-            zooming(this, dir);
+            zooming(dir);
             return false;
         });
 
         //鼠标滚轮缩放图片
-        function zooming(o, delta) {
+        function zooming(delta) {
             if (delta == 'up') {
                 zoom_n -= 0.2;
                 zoom_n = zoom_n <= 0.2 ? 0.2 : zoom_n;
@@ -153,31 +154,58 @@ function imgView(domName, parentsName) {
             });
         }
 
+
+
         /* 上一张 */
-        $(".icon-arrow-left").on("click", function () {
+        $(".icon-arrow-left").click(function () {
+            $(this).stop(true, false);
+            $(this).animate({ marginLeft: "-15px" }, 250);
+            $(this).animate({ marginLeft: "0px" }, 250);
+            lastPic();
+        });
+        /* 下一张 */
+        $(".icon-arrow-right").click(function () {
+            $(this).stop(true, false);
+            $(this).animate({ marginLeft: "15px" }, 250);
+            $(this).animate({ marginLeft: "0px" }, 250);
+            nextPic();
+        });
+        function lastPic() {/* 上一张 */
             img_index--;
             if (img_index == -1) {
                 img_index = len - 1;
             }
-            $img_container.removeClass("transitionImg");
             imgReset();
             loadImg();//加载图片，显示当前选中
-        });
-        /* 下一张 */
-        $(".icon-arrow-right").on("click", function () {
+        };
+        function nextPic() {/* 下一张 */
             img_index++;
             if (img_index == len) {
                 img_index = 0;
             }
+
             imgReset();
             loadImg();//加载图片，显示当前选中
-        });
+        }
+
 
         /* 旋转 */
+        let spin_xiangyou = 0;
         $(".icon-xiangyouxuanzhuan").click(function () {
+            spin_xiangyou += 360;
+            $(this).css({
+                "transform": "rotate(" + spin_xiangyou + "deg)",
+                "transition": "transform 500ms"
+            });
             clockwise(); //顺时针旋转
         });
+        let spin_xiangzuo = 0;
         $(".icon-xiangzuoxuanzhuan").click(function () {
+            spin_xiangzuo -= 360;
+            $(this).css({
+                "transform": "rotate(" + spin_xiangzuo + "deg)",
+                "transition": "transform 500ms"
+            });
             counterClockwise(); //逆时针旋转
         })
         /*顺时针旋转*/
@@ -197,32 +225,39 @@ function imgView(domName, parentsName) {
 
 
         /*空白关闭*/
-        $('.mask-layer').click(function () {
-            $(document).click(function (e) { // 在页面任意位置点击而触发此事件
-                var cname = $(e.target).attr('class');
-                if (cname == 'mask-layer') {
-                    removeMask();
-                }
-            })
-        });
+        $(".mask-layer").click(function (event) {
+            if ($(event.target).hasClass("mask-layer")) {
+                removeMask();
+            }
+        })
         /* 按钮关闭 */
         $('.icon-guanbi').click(function () {
             removeMask();
         });
+
         /* 遮罩层关闭 */
         function removeMask() {
             $('.mask-layer').remove();
+            $(document).unbind("keydown");/* 给按键解绑 */
         }
 
 
         /*新窗口打开 */
         $('.icon-open-blank').click(function () {
-            window.open("http://127.0.0.1:5500/viewer/" + arrPic[img_index]);
+            $(this).stop(true, false);
+            $(this).animate({ marginLeft: "10px" });
+            $(this).animate({ marginLeft: "0px" });
+            window.open(arrPic[img_index]);
         });
 
-
+        let spin_shuaxin = 0;
         /* 图片还原 */
-        $('.icon-huanyuan').click(function () {
+        $('.icon-shuaxin').click(function () {
+            spin_shuaxin += 360;
+            $(this).css({
+                "transform": "rotate(" + spin_shuaxin + "deg)",
+                "transition": "transform 500ms"
+            });
             imgReset();
         });
         /* 还原方法 */
@@ -243,11 +278,44 @@ function imgView(domName, parentsName) {
                 }
             }
             zoom_n = 1;
-            $img_container.animate({ left: "50%", top: "50%" }, 500);
+            $($img_container).stop(false, false);
+            $img_container.animate({ left: "50%", top: "50%" }, 400);
+            /* 用400ms是为了重复还原图片时停止动画后的效果，不至于每次重复点击后还需要500ms，时间太长 */
             $img_container.css({
                 "transform": "translate(-50%, -50%) rotate(" + spin_n + "deg) scale(" + zoom_n + ")"
             });
-        }
-    }
+        };
 
+        //按键功能
+        /* 最后没解绑的话，每次点开图片打开遮罩层都会重新绑定，这样按一次按键就会触发多一次事件，会依次增加，
+         * 因此，每次关闭遮罩层的时候需要对此解绑*/
+        $(document).bind("keydown", function (event) {
+            let speed = 20;
+            switch (event.key) {
+                case "ArrowRight": {
+                    if (event.ctrlKey) { $img_container.css({ "left": $img_container[0].offsetLeft + speed + "px" }); }
+                    else { lastPic(); } break;
+                }
+                case "ArrowLeft": {
+                    if (event.ctrlKey) { $img_container.css({ "left": $img_container[0].offsetLeft - speed + "px" }); }
+                    else { nextPic(); } break;
+                }
+                case "ArrowUp": {
+                    if (event.ctrlKey) { $img_container.css({ "top": $img_container[0].offsetTop - speed + "px" }); }
+                    else { zooming("down"); } break;
+                }
+                case "ArrowDown": {
+                    if (event.ctrlKey) { $img_container.css({ "top": $img_container[0].offsetTop + speed + "px" }); }
+                    else { zooming("up"); } break;
+                }
+                case "PageUp": clockwise(); break;  //顺时针旋转
+                case "PageDown": counterClockwise(); break; //逆时针旋转; 
+                case "Escape": removeMask(); break;
+                case "F12": return true; break;
+                // default: alert(event.key)
+            }
+            return false;//取消默认
+        });
+
+    }
 }
